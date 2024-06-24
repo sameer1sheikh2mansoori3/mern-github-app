04f4b166415033329828b30053f834393ef69be9
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2024-06-24 15:37:15 +0530

    no meaning message

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HomePage = () => {
+	return (
+		<div className='m-4'>
+			{/* <Search />
+			<SortRepos /> */}
+			<div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
+				{/* <ProfileInfo />
+				<Repos />
+				<Spinner /> */}
+			</div>
+		</div>
+	);
+};
+
+export default HomePage
