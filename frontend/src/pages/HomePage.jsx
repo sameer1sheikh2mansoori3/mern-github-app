cc88ad6b1958e7873c95e67308b835aed007bc58
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2024-06-24 15:50:02 +0530

    all add

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-
+import Search from '../components/Search';
+import SortRepos from '../components/SortRepos';
+import ProfileInfo from '../components/ProfileInfo';
+import Repos from '../components/Repos';
+import Spinner from '../components/Spinner';
 const HomePage = () => {
 	return (
 		<div className='m-4'>
-			{/* <Search />
-			<SortRepos /> */}
+			<Search />
+			<SortRepos />
 			<div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
-				{/* <ProfileInfo />
+				<ProfileInfo />
 				<Repos />
-				<Spinner /> */}
+				<Spinner />
 			</div>
 		</div>
 	);
